3470949a917170178e1328cab39f60d64db572f8
caleb7hayes <55815986+caleb7hayes@users.noreply.github.com>
2022-04-05 14:37:57 -0600

    Add Workspace

---
+++ MotivityV2/MotivityV2/MotivityV2App.swift
@@ -0,0 +1,17 @@
+//
+//  MotivityV2App.swift
+//  MotivityV2
+//
+//  Created by Tyler on 4/5/22.
+//
+
+import SwiftUI
+
+@main
+struct MotivityV2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
